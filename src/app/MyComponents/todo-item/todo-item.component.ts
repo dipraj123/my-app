51795cf2096eb2d503ec422fa8253652e6f28197
‘dipraj123’ <goswamidipraj@gmail.com>
2024-09-25 02:11:03 +0530

    TodoList Added

---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from '../../Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+  @Input() todo!:Todo;
+
+  constructor()
+  {
+
+  }
+
+  onClick=()=>
+  {
+    console.log("I am clicked");
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './MyComponents/todo/todo.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
+    TodoItemComponent,
 
   ],
   imports: [
